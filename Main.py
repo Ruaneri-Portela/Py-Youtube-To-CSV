4368336fd7521ddd7f7e9945c5da58414f718842
Ruaneri Portela <ruaneriportela@furg.br>
2022-09-17 04:12:11 -0300

    sync

--- Main.py
+++ Main.py
@@ -1,21 +1,21 @@
 import YoutubeApi
 import Login
-import sys
 
 returnVar = True
 loginMode = None
 # Connect To YoutubeAPI
 
 
-def getVideos(playlistId, mode):
+def getVideos(playlistId, mode, listIndex):
     youtubeImport = YoutubeApi.dataVideo
-    return youtubeImport.getData(youtube, playlistId, mode)
+    return youtubeImport.getData(youtube, playlistId, mode, listIndex)
 
 
 # Main Script loop
 def main():
+    listIndex = 0
     listVideo = []
-    headerCSV = "Titulo,Visualizações,Gosteis,Não Gosteis,Comentarios,Duração,Data De Publicação,Link\n"
+    headerCSV = "#|Titulo|Visualizações|Gosteis|Não Gosteis|Comentarios|Duração|Data De Publicação|Link\n"
     # Header
     print("#######################\nBem vindo ao importador de dados de videos e playlist do youtube")
     while True:
@@ -31,7 +31,8 @@ def main():
                 break
             else:
                 try:
-                    listVideo.extend(getVideos(comandVar, 0))
+                    listVideo.extend(getVideos(comandVar, 0, listIndex))
+                    listIndex = len(listVideo)
                 except:
                     print("Ocorreu um erro! Tente novamente.")
         # Append Single Video To List
@@ -43,7 +44,8 @@ def main():
                 break
             else:
                 try:
-                    listVideo.extend(getVideos(comandVar, 1))
+                    listVideo.extend(getVideos(comandVar, 1, listIndex))
+                    listIndex = len(listVideo)
                 except:
                     print("Ocorreu um erro! Tente novamente.")
         # View Video List
@@ -54,7 +56,7 @@ def main():
         # Export To CSV File
         elif (comandVar == "Exportar dados para um arquivo") or (comandVar == "4"):
             comandVar = input(
-                "Qual o nome do arquivo? ATENÇÃO caso o arquivo exista ele vai ser substituido\n#Exportar lista~>")
+                "Criando Arquivo\nQual o nome do arquivo? ATENÇÃO caso o arquivo exista ele vai ser substituido\n#Exportar lista~>")
             if (comandVar == "Retornar") or (comandVar == "9"):
                 pass
             elif (comandVar == "Sair") or (comandVar == "0"):
@@ -71,13 +73,32 @@ def main():
                     "Exportador de dados do youtube para planilha eletronica. Criador por Ruaneri Portela/Brasil 2022")
                 csv.close()
             print("Dados exportados em", comandVar+".csv")
+        # Get all data
+        elif (comandVar == "Todos Os Dados") or (comandVar == "6"):
+            indexVar = input(
+                "#~>Qual o video da lista que você deseja mais informações? ")
+            if (comandVar == "Retornar") or (comandVar == "9"):
+                pass
+            elif (comandVar == "Sair") or (comandVar == "0"):
+                break
+            else:
+                try:
+                    indexVar = (int)(indexVar)
+                    print("Mostrando todos os dados de -> ",
+                          (listVideo[indexVar][1]), "\n")
+                    stringRemove = str(listVideo[indexVar][8])
+                    id = str(stringRemove.replace("https://youtu.be/", ""))
+                    print(YoutubeApi.dataVideo.allData(youtube, id))
+                except:
+                    print("Ocorreu um erro! Tente novamente.")
         # Clean Video List Data
         elif (comandVar == "Limpar Lista") or (comandVar == "7"):
             listVideo = []
+            listIndex = 0
             print("Lista limpa!")
         # Help informations
         elif (comandVar == "Ajuda") or (comandVar == "8"):
-            print("Tela de Ajuda:\n1 -> Ler uma Playlist\n2 -> Ler um video\n3 -> Listar videos importados\n4 -> Exportar para um arquivo os dados dos videos importados\n7 -> Limpar lista de videos importados\n8 -> Ajuda \n9 -> Retornar para tela de login\n0 -> Sair\n ")
+            print("Tela de Ajuda:\n1 -> Ler uma Playlist\n2 -> Ler um video\n3 -> Listar videos importados\n4 -> Exportar para um arquivo os dados dos videos importados\n6 -> Obter todos os dados de um video na lista\n7 -> Limpar lista de videos importados\n8 -> Ajuda \n9 -> Retornar para tela de login\n0 -> Sair\n ")
         # Return to login
         elif (comandVar == "Login") or (comandVar == "9"):
             print("Voltando para tela de login")
